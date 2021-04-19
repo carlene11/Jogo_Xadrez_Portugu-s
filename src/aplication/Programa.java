98e39ba994e8f8a9b8380e8af5b9b601dd6905a8
Carlene Maria Schlemper <carlenemari@gmail.com>
2021-04-19 16:07:06 -0300

    Projeto criado

---
+++ src/aplication/Programa.java
@@ -0,0 +1,12 @@
+package aplication;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+//JOGO DE XADREZ - PROGRAMA PRINCIPAL
+		System.out.println("Jogo de Xadrez");
+
+	}
+
+}
