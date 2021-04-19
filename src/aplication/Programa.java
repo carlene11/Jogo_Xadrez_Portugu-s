225f55dc9ad1da033dedf4971fee1b103b86c4b5
Carlene Maria Schlemper <carlenemari@gmail.com>
2021-04-19 16:16:35 -0300

    Projeto criado em Português

--- src/aplication/Programa.java
+++ src/aplication/Programa.java
@@ -5,7 +5,7 @@ public class Programa {
 	public static void main(String[] args) {
 
 //JOGO DE XADREZ - PROGRAMA PRINCIPAL
-		System.out.println("Jogo de Xadrez");
+		System.out.println("Jogo de Xadrez em Portugues");
 
 	}
 
